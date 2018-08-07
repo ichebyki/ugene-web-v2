a4862070c256b3d031b78317df5bd871f49a78f6
Igor Chebykin <ichebyki@gmail.com>
2018-08-07 22:03:20 +0700

    add mui

--- src/main/app/src/containers/about/index.js
+++ src/main/app/src/containers/about/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => (
   <div>
     <h1>About Page</h1>
-    <p>Boilerplate application to demonstrate how to wire up Spring, JWT Authentication, React, Redux and Websockets</p>
+    <p>@Boilerplate application to demonstrate how to wire up Spring, JWT Authentication, React, Redux and Websockets</p>
   </div>
 );
 
